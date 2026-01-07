8139bc1940452fbff401f9b6a5ba83db75ef95d2
Alicia <alispagueti@gmail.com>
2026-01-07 16:08:17 +0100

    documentacion java

--- cuentas/CCuenta.java
+++ cuentas/CCuenta.java
@@ -1,42 +1,68 @@
 package cuentas;
 
+/**
+ * Clase que representa una cuenta bancaria.
+ */
 public class CCuenta {
 
-
+    /**
+     * Nombre del titular de la cuenta.
+     */
     private String nombre;
+
+    /**
+     * Número o identificador de la cuenta.
+     */
     private String cuenta;
+
+    /**
+     * Saldo actual disponible en la cuenta.
+     */
     private double saldo;
+
+    /**
+     * Tipo de interés aplicado a la cuenta.
+     */
     private double tipoInterés;
 
-    public CCuenta()
-    {
+    /**
+     * Constructor vacío.
+     * Inicializa una cuenta sin datos.
+     */
+    public CCuenta() {
     }
 
-    public CCuenta(String nom, String cue, double sal, double tipo)
-    {
-        nombre =nom;
-        cuenta=cue;
-        saldo=sal;
+    /**
+     * Constructor con datos.
+     *
+     * @param nom  nombre del titular
+     * @param cue  identificador o número de cuenta
+     * @param sal  saldo inicial
+     * @param tipo tipo de interés
+     */
+    public CCuenta(String nom, String cue, double sal, double tipo) {
+        nombre = nom;
+        cuenta = cue;
+        saldo = sal;
     }
 
-    public double estado()
-    {
+    /**
+     * Devuelve el saldo actual de la cuenta.
+     *
+     * @return saldo actual
+     */
+    public double estado() {
         return saldo;
     }
 
-    public void ingresar(double cantidad) throws Exception
-    {
-        if (cantidad<0)
+    /**
+     * Ingresa una cantidad en la cuenta.
+     *
+     * @param cantidad importe a ingresar
+     * @throws Exception si la cantidad es negativa
+     */
+    public void ingresar(double cantidad) throws Exception {
+        if (cantidad < 0)
             throw new Exception("No se puede ingresar una cantidad negativa");
-        saldo = saldo + cantidad;
-    }
+        saldo = saldo + ca
 
-    public void retirar(double cantidad) throws Exception
-    {
-        if (cantidad <= 0)
-            throw new Exception ("No se puede retirar una cantidad negativa");
-        if (estado()< cantidad)
-            throw new Exception ("No se hay suficiente saldo");
-        saldo = saldo - cantidad;
-    }
-}
